44c36e9df257b34c5a26ce309cdd71a5c722bd10
ARGreat <algo8048@colorado.edu>
2025-10-14 08:25:31 -0600

    Initial Commit

---
+++ Main.m
@@ -0,0 +1,9 @@
+%Main for ASEN-3801 Lab 4 Quadcopter Lab
+%Date: 10/14/2025
+%Authors: Alex Godbout, Christopher Westerlund, Damien Wilson, Maria Cristina Sarno
+%Inputs: RSdata_nocontrol.mat
+%Outputs:
+clear; clc; close all;
+
+%Load in Control File
+load('RSdata_nocontrol.mat');
