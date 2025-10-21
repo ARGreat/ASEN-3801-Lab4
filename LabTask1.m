38043bcaacb6e4fef90f933e49404574dc5a81b9
ARGreat <algo8048@colorado.edu>
2025-10-21 09:04:28 -0600

    Made alot of changes

--- LabTask1.m
+++ LabTask1.m
@@ -18,14 +18,14 @@ d = 0.06; %m
 km = 0.0024; %N*m/N
 I_x = 5.8*10^(-5); %kgm^2
 I_y = 7.2*10^(-5); %kgm^2
-I_z = 1*10^4; %kgm^2
+I_z = 1*10^(-4); %kgm^2
 I = [I_x;I_y;I_z];
 nu = 1*10^(-3); %N/(m/s)^2
 mu = 2*10^(-6); %N*m/(rad/s)^2
 
 %Calculating Trim Cons for 1.4
 phi = atan((nu * 5^2) / (m * g));
-Zc = (m * g) / cos(phi);
+fc = (m * g) / cos(phi) / 4;
 
 
 %% 1.2 Run QuadrotorEOM for Aircraft in Steady Hover without Aero Forces
@@ -38,7 +38,7 @@ motor_forces = m*g*[0.25;0.25;0.25;0.25];
 n = length(time);
 control_input_array = repmat(motor_forces,1,n); %Needs to be Stretched to length n for plotting
 fig = [11;12;13;14;15;16];
-col = ["-b";"-b";"-b";"-b";"-b";"-b"];
+col = ["";"";"";"";"";""];
 PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, col);
 
 %% 1.3 Run Quadrotor EOM with Aero Forces
@@ -58,8 +58,8 @@ PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, col);
 %% 1.4 Run Quadrotor EOM while traveling 5m/s East;
     
     %Not yawed
-        var_0 = [0;0;-2;5;0;0;0;0;0;0;0;0]; %State Vector for Quadrotor in Steady Hover;
-        motor_forces = [0;0;0;0];
+        var_0 = [0;0;-2;0.0375;0;0;5;0;0;0;0;0]; %State Vector for Quadrotor in Steady Hover;
+        motor_forces = -[0.1669;0.1669;0.1669;0.1669];
         [time,aircraft_state_array] = ode45(@(t,aircraft_state_array) QuadrotorEOM(t, aircraft_state_array, g, m, I, d, km, nu, mu, motor_forces), [0,10],var_0);
     
         % up For Plotting
@@ -70,7 +70,7 @@ PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, col);
         PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, col)
 
     %yawed
-        var_0 = [0;0;-2;5;0;0;pi/2;0;0;0;0;0]; %State Vector for Quadrotor in Steady Hover;
+        var_0 = [0;0;-2;0;0;pi/2;05;0;0;0;0;0]; %State Vector for Quadrotor in Steady Hover;
         motor_forces = [0;0;0;0];
         [time,aircraft_state_array] = ode45(@(t,aircraft_state_array) QuadrotorEOM(t, aircraft_state_array, g, m, I, d, km, nu, mu, motor_forces), [0,10],var_0);
     
