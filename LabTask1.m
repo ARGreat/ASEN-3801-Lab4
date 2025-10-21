249e39e7d8a5bb22dc6791d4407e665281b327cc
ARGreat <algo8048@colorado.edu>
2025-10-21 09:46:56 -0600

    Makes les figuress

--- LabTask1.m
+++ LabTask1.m
@@ -32,7 +32,7 @@ motor_forces = m*g*[0.25;0.25;0.25;0.25];
 %Set up for Plotting
 n = length(time);
 control_input_array = repmat(motor_forces,1,n); %Needs to be Stretched to length n for plotting
-fig = [11;12;13;14;15;16];
+fig = [11;12;13;-1;-1;-1];
 col = ["";"";"";"";"";""];
 PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, col);
 
@@ -46,7 +46,7 @@ PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, col);
     %Set up For Plotting
     n = length(time);
     control_input_array = repmat(motor_forces,1,n); %Needs to be Stretched to length n for plotting
-    fig = [21;22;23;24;25;26];
+    fig = [21;22;23;-1;-1;-1];
     col = ["";"";"";"";"";""];
     PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, col)
 
@@ -63,19 +63,19 @@ PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, col);
         % up For Plotting
         n = length(time);
         control_input_array = repmat(motor_forces,1,n); %Needs to be Stretched to length n for plotting
-        fig = [31;32;33;34;35;36];
+        fig = [31;32;33;-1;-1;-1];
         col = ["";"";"";"";"";""];
         PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, col)
 
-    %yawed
-        var_0 = [0;0;-2;0;0;pi/2;05;0;0;0;0;0]; %State Vector for Quadrotor in Steady Hover;
-        motor_forces = [0;0;0;0];
+    %yawed      [x,y, z,φ,  θ,   ψ,         u,v,          w,p,q,r]
+        var_0 = [0;0;-2;0;-phi;pi/2;5*cos(phi);0;-5*sin(phi);0;0;0]; %State Vector for Quadrotor in Steady Hover;
+        motor_forces = [fc;fc;fc;fc];
         [time,aircraft_state_array] = ode45(@(t,aircraft_state_array) QuadrotorEOM(t, aircraft_state_array, g, m, I, d, km, nu, mu, motor_forces), [0,10],var_0);
     
         % up For Plotting
         n = length(time);
         control_input_array = repmat(motor_forces,1,n); %Needs to be Stretched to length n for plotting
-        fig = [41;42;43;44;45;46];
+        fig = [41;42;43;-1;-1;-1];
         col = ["";"";"";"";"";""];
         PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, col)
 end
--- PlotAircraftSim.m
+++ PlotAircraftSim.m
@@ -25,85 +25,93 @@ function PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, co
 % [x, y, z, φ, θ, ψ, u, v, w, p, q, r] [𝑍𝑍𝑐𝑐, 𝐿𝐿 𝑐𝑐, 𝑀𝑀𝑐𝑐, 𝑁𝑁𝑐𝑐]𝑇𝑇
 
 % Intertial Position
-figure(fig(1));hold on;
-sgtitle("Inertial Positon (m) vs Time (s)")
-
-subplot(3,1,1);hold on;
-plot(time, aircraft_state_array(:,1), col(1)); ylabel('x (m)'); hold off;
-
-subplot(3,1,2);hold on;
-plot(time, aircraft_state_array(:,2), col(1)); hold on; ylabel('y (m)')
-
-subplot(3,1,3);hold on;
-plot(time, aircraft_state_array(:,3), col(1)); hold on; ylabel('z (m)'); xlabel('Time (s)');
-hold off
+if(fig(1) >=1)
+    figure(fig(1));hold on;
+    sgtitle("Inertial Positon (m) vs Time (s)")
+    
+    subplot(3,1,1);hold on;
+    plot(time, aircraft_state_array(:,1), col(1)); ylabel('x (m)'); hold off;
+    
+    subplot(3,1,2);hold on;
+    plot(time, aircraft_state_array(:,2), col(1)); hold on; ylabel('y (m)')
+    
+    subplot(3,1,3);hold on;
+    plot(time, aircraft_state_array(:,3), col(1)); hold on; ylabel('z (m)'); xlabel('Time (s)');
+    hold off
+end
 %Euler Angles
-figure(fig(2)); hold on;
-sgtitle("Euler Angles (rad) vs Time (s)")
-
-subplot(3,1,1);
-plot(time, aircraft_state_array(:,4), col(2)); hold on; ylabel('φ (rad)')
-
-subplot(3,1,2);
-plot(time, aircraft_state_array(:,5), col(2)); hold on; ylabel('θ (rad)')
-
-subplot(3,1,3);
-plot(time, aircraft_state_array(:,6), col(2)); hold on; ylabel('ψ (rad)'); xlabel('Time (s)');
-hold off
-
+if(fig(2) >=1)
+    figure(fig(2)); hold on;
+    sgtitle("Euler Angles (rad) vs Time (s)")
+    
+    subplot(3,1,1);
+    plot(time, aircraft_state_array(:,4), col(2)); hold on; ylabel('φ (rad)')
+    
+    subplot(3,1,2);
+    plot(time, aircraft_state_array(:,5), col(2)); hold on; ylabel('θ (rad)')
+    
+    subplot(3,1,3);
+    plot(time, aircraft_state_array(:,6), col(2)); hold on; ylabel('ψ (rad)'); xlabel('Time (s)');
+    hold off
+end
 %Inertial Velocity
-figure(fig(3));
-sgtitle("Inertial Velocity (m/s) vs Time (s)")
-
-subplot(3,1,1);
-plot(time, aircraft_state_array(:,7), col(3)); hold on; ylabel('u (m/s)')
-
-subplot(3,1,2);
-plot(time, aircraft_state_array(:,8), col(3)); hold on; ylabel('v (m/s)')
-
-subplot(3,1,3);
-plot(time, aircraft_state_array(:,9), col(3)); hold on; ylabel('w (m/s)'); xlabel('Time (s)');
-hold off
-
+if(fig(3) >=1)
+    figure(fig(3));
+    sgtitle("Inertial Velocity (m/s) vs Time (s)")
+    
+    subplot(3,1,1);
+    plot(time, aircraft_state_array(:,7), col(3)); hold on; ylabel('u (m/s)')
+    
+    subplot(3,1,2);
+    plot(time, aircraft_state_array(:,8), col(3)); hold on; ylabel('v (m/s)')
+    
+    subplot(3,1,3);
+    plot(time, aircraft_state_array(:,9), col(3)); hold on; ylabel('w (m/s)'); xlabel('Time (s)');
+    hold off
+end
 %Angular Velocity
-figure(fig(4));
-sgtitle("Angular Velocity (rad/s) vs Time (s)");
-
-subplot(3,1,1);
-plot(time, aircraft_state_array(:,10), col(4)); hold on; ylabel('p (rad/s)')
-
-subplot(3,1,2);
-plot(time, aircraft_state_array(:,11), col(4)); hold on; ylabel('q (rad/s)')
-
-subplot(3,1,3);
-plot(time, aircraft_state_array(:,12), col(4)); hold on; ylabel('r (rad/s)'); xlabel('Time (s)');
-hold off
-
+if(fig(4) >=1)
+    figure(fig(4));
+    sgtitle("Angular Velocity (rad/s) vs Time (s)");
+    
+    subplot(3,1,1);
+    plot(time, aircraft_state_array(:,10), col(4)); hold on; ylabel('p (rad/s)')
+    
+    subplot(3,1,2);
+    plot(time, aircraft_state_array(:,11), col(4)); hold on; ylabel('q (rad/s)')
+    
+    subplot(3,1,3);
+    plot(time, aircraft_state_array(:,12), col(4)); hold on; ylabel('r (rad/s)'); xlabel('Time (s)');
+    hold off
+end
 
 %Control Variables
-figure(fig(5));
-sgtitle("Control Variables (N) vs Time (s)");
-
-subplot(4,1,1);
-plot(time, control_input_array(1,:), col(5)); hold on; ylabel('Z_c (N)')
-
-subplot(4,1,2); 
-plot(time, control_input_array(2,:), col(5)); hold on; ylabel('L_c (N)')
-
-subplot(4,1,3); 
-plot(time, control_input_array(3,:), col(5)); hold on; ylabel('M_c (N)')
-
-subplot(4,1,4);
-plot(time, control_input_array(4,:), col(5)); hold on; ylabel('N_c (N)'); xlabel('Time (s)');
-hold off;
+if(fig(5) >=1)
+    figure(fig(5));
+    sgtitle("Control Variables (N) vs Time (s)");
+    
+    subplot(4,1,1);
+    plot(time, control_input_array(1,:), col(5)); hold on; ylabel('Z_c (N)')
+    
+    subplot(4,1,2); 
+    plot(time, control_input_array(2,:), col(5)); hold on; ylabel('L_c (N)')
+    
+    subplot(4,1,3); 
+    plot(time, control_input_array(3,:), col(5)); hold on; ylabel('M_c (N)')
+    
+    subplot(4,1,4);
+    plot(time, control_input_array(4,:), col(5)); hold on; ylabel('N_c (N)'); xlabel('Time (s)');
+    hold off;
+end
 %3-d Path
-figure(fig(6));
-view(3);
-
-plot3(aircraft_state_array(:,1),aircraft_state_array(:,2),aircraft_state_array(:,3), col(6)); hold on;
-plot3(aircraft_state_array(1,1),aircraft_state_array(1,2),aircraft_state_array(1,3),'go', 'MarkerFaceColor', 'g'); hold on;
-plot3(aircraft_state_array(end,1),aircraft_state_array(end,2),aircraft_state_array(end,3),'ro', 'MarkerFaceColor', 'r'); hold on;
-title("3D Path");
-
+if(fig(6) >=1)
+    figure(fig(6));
+    view(3);
+    
+    plot3(aircraft_state_array(:,1),aircraft_state_array(:,2),aircraft_state_array(:,3), col(6)); hold on;
+    plot3(aircraft_state_array(1,1),aircraft_state_array(1,2),aircraft_state_array(1,3),'go', 'MarkerFaceColor', 'g'); hold on;
+    plot3(aircraft_state_array(end,1),aircraft_state_array(end,2),aircraft_state_array(end,3),'ro', 'MarkerFaceColor', 'r'); hold on;
+    title("3D Path");
+end
 end
 
